0dabbd8f08a7366dbceea9b29eaf4be579b4be57
Tanmay R Suradkar <tanmaysuradkar2008@gmail.com>
2026-07-05 13:26:13 +0530

    set main url

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio-website',
+  assetPrefix: '/portfolio-website/',
+  images: {
+    unoptimized: true, // REQUIRED for GitHub Pages
+  },
 };
 
 export default nextConfig;
